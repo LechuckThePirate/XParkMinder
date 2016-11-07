b7b8c45a142c872b355654bffbefecd2fef15849
Joan <joan.vilarino@gmail.com>
2016-11-07 14:27:36 +0100

    Re-Added Windows UWP project. Still having trouble with System.Runtime 4.1.0 not being found :(

---
+++ XParkMinder/XParkMinder.UWP/NativeServices/UwpGpsService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XParkMinder.Geo.Contracts.ServiceLibrary.Contracts;
+using XParkMinder.Geo.Contracts.ServiceLibrary.DTO;
+
+namespace XParkMinder.UWP.NativeServices
+{
+    public class UwpGpsService : IGpsService
+    {
+        public async Task<PortableLocation> GetGpsCurrentLocation()
+        {
+            return await Task.FromResult<PortableLocation>(null);
+        }
+    }
+}
